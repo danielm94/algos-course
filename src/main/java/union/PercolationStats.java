bf244ebd3b00f1e04be89c1cee66a3a8d8acb046
Daniel Martins <24377240+danielm94@users.noreply.github.com>
2023-04-30 23:14:57 -0400

    Add StdStats methods, add constant for confidence, removed unused field.

--- src/main/java/union/PercolationStats.java
+++ src/main/java/union/PercolationStats.java
@@ -1,14 +1,14 @@
+package union;
+
 import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
 
 public class PercolationStats {
+    private static final double CONFIDENCE_95 = 1.96;
     private final int n;
     private final int trials;
 
     private final double[] trialFractions;
-
-    private double trialFractionsSum;
-
-    private Double mean;
 
     // perform independent trials on an n-by-n grid
     public PercolationStats(int n, int trials) {
@@ -32,33 +32,27 @@ public class PercolationStats {
             }
             double fraction = (double) p.numberOfOpenSites() / (n * n);
             trialFractions[i] = fraction;
-            trialFractionsSum += fraction;
         }
     }
 
     // sample mean of percolation threshold
     public double mean() {
-        return mean == null ? mean = trialFractionsSum / trials : mean;
+        return StdStats.mean(trialFractions);
     }
 
     // sample standard deviation of percolation threshold
     public double stddev() {
-        double sum = 0;
-        double mean = mean();
-        for (double fraction : trialFractions) {
-            sum += (fraction - mean) * (fraction - mean);
-        }
-        return sum / (trials - 1);
+        return StdStats.stddev(trialFractions);
     }
 
     // low endpoint of 95% confidence interval
     public double confidenceLo() {
-        return mean - ((1.96 * stddev()) / Math.sqrt(trials));
+        return mean() - ((CONFIDENCE_95 * stddev()) / Math.sqrt(trials));
     }
 
     // high endpoint of 95% confidence interval
     public double confidenceHi() {
-        return mean + ((1.96 * stddev()) / Math.sqrt(trials));
+        return mean() + ((CONFIDENCE_95 * stddev()) / Math.sqrt(trials));
     }
 
     // test client (see below)
