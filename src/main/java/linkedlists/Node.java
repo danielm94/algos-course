a452d6d7ecdce5117366cca8ebb86cce52d47b36
Daniel Martins <24377240+danielm94@users.noreply.github.com>
2023-05-21 17:31:48 -0400

    Change Node class so it supports generics.

--- src/main/java/linkedlists/Node.java
+++ src/main/java/linkedlists/Node.java
@@ -1,32 +1,55 @@
 package linkedlists;
 
-public class Node {
-    private String item;
-    private Node next;
+import java.util.Iterator;
+
+public class Node<T> implements Iterable<T> {
+    private T item;
+    private Node<T> next;
 
     public Node() {
         this.item = null;
         this.next = null;
     }
 
-    public Node(String item, Node next) {
+    public Node(T item, Node<T> next) {
         this.item = item;
         this.next = next;
     }
 
-    public String getItem() {
+    public T getItem() {
         return item;
     }
 
-    public void setItem(String item) {
+    public void setItem(T item) {
         this.item = item;
     }
 
-    public Node getNext() {
+    public Node<T> getNext() {
         return next;
     }
 
-    public void setNext(Node next) {
+    public void setNext(Node<T> next) {
         this.next = next;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new NodeIterator();
+    }
+
+    private class NodeIterator implements Iterator<T> {
+        private Node<T> head = Node.this;
+
+        @Override
+        public boolean hasNext() {
+            return head != null;
+        }
+
+        @Override
+        public T next() {
+            T item = head.item;
+            head = head.next;
+            return item;
+        }
     }
 }
